1a544a8d55346a89e651edef1597bcf55b095c61
sharryheb <43225557+sharryheb@users.noreply.github.com>
2019-10-02 22:55:08 -0700

    fix PORT env var

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
 
 // Initialize Express
